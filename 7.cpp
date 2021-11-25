abe9f252c0a99cc5513d35d7da0b00cce5fe3b62
11062004Maksim11 <11maksss.maksss.maksss06@gmail.com>
2021-11-26 00:24:07 +0200

    Based

---
+++ 7.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	int x, k = 5;
+	cout << "Enter x =";
+	cin >> x;
+	float rez;
+	float Z = 1;
+	for (int n = 1; n < 6; n++)
+	{
+		Z =   Z  + log(n * x);
+	}
+	rez = tan(x)  + Z;
+	cout << "Z = " << Z << endl;
+
+	system("pause");
+	return 0;
+}
+
